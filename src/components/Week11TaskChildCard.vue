0a18ac4618dd7063683a94130b613f8cb1aefd70
Uchechukwu-best <uchman6122@gmail.com>
2026-07-24 07:46:00 +0100

    Worked on the card component

--- src/components/Week11TaskChildCard.vue
+++ src/components/Week11TaskChildCard.vue
@@ -7,6 +7,7 @@ const props = defineProps({
   hoverable: Boolean,
   src: {
     type: String,
+    default: null,
   },
   alt: {
     type: String,
@@ -14,16 +15,33 @@ const props = defineProps({
   variant: {
     type: String,
     default: "default",
-    require: true,
-    validator: () => {
-      return ["default", "bordered", "elevated"];
+    validator: (value) => {
+      return ["default", "bordered", "elevated"].includes(value);
     },
   },
 });
+
+const cardClasses = {
+  base: "rounded-lg overflow-hidden bg-gray-300 transition-shadow grid place-items-center text-center mt-5 p-2",
+  variant: {
+    default: "",
+    bordered: "border-2 border-black",
+    elevated: "shadow-lg",
+  },
+  hoverable: "hover:shadow-xl hover:-translate-y-1 transition-transform",
+};
 </script>
 
 <template>
-  <div>
-    <h1>{{ title }}</h1>
+  <div
+    :class="[
+      cardClasses.base,
+      cardClasses.variant[variant],
+      { [cardClasses.hoverable]: hoverable },
+    ]"
+  >
+    <img v-if="src" :src="src" :alt="alt" class="w-70" />
+    <h1 class="text-3xl font-bold my-3">{{ title }}</h1>
+    <slot></slot>
   </div>
 </template>
--- src/components/Week11TaskParent.vue
+++ src/components/Week11TaskParent.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Week11TaskChildButton from "./Week11TaskChildButton.vue";
+import Week11TaskChildCard from "./Week11TaskChildCard.vue";
+import img from "../assets/refvsreactive.png";
 function handleClick(message) {
   console.log(message);
 }
 </script>
 
 <template>
-  <div class="inline-flex gap-5">
+  <div class="inline-flex gap-5 border-2 m-5">
     <Week11TaskChildButton size="small" variant="primary" @click="handleClick"
       >Primary</Week11TaskChildButton
     >
@@ -16,5 +18,40 @@ function handleClick(message) {
     <Week11TaskChildButton size="large" variant="danger" :disabled="true"
       >Danger</Week11TaskChildButton
     >
+  </div>
+
+  <div class="flex gap-5 m-auto max-w-5xl">
+    <Week11TaskChildCard
+      title="React Dev"
+      variant="bordered"
+      class="max-w-md"
+      hoverable
+    >
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+        eveniet.
+      </p>
+    </Week11TaskChildCard>
+
+    <Week11TaskChildCard
+      title="Ref Vs Reactive"
+      :src="img"
+      :alt="Userimage"
+      variant="elevated"
+      class="max-w-md"
+      hoverable
+    >
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+        eveniet.
+      </p>
+    </Week11TaskChildCard>
+
+    <Week11TaskChildCard title="Vue Dev" class="max-w-md" hoverable>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+        eveniet.
+      </p>
+    </Week11TaskChildCard>
   </div>
 </template>
